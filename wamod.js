9de40debea4034d8f5c5557db7921020104d5837
BlackAmda <sl.amdakolla@gmail.com>
2023-01-24 13:04:56 +0530

    1.0.0v

--- wamod.js
+++ wamod.js
@@ -1,24 +1,26 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+
 async function mods() {
-const list = []
-axios.get("https://fmmods.com/download-center/mega.php").then(urlResponse => {
-    const $ = cheerio.load(urlResponse.data)
-    $('div.su-button-center').each((i,element)=> {
-        const link = $(element).find("a").attr("href");
-        list.push({
-            name: link.split('/')[4].replace('.', '_').replace('_apk', '.apk'),
-            link: link
-        });
+    const list = []
+    axios.get("https://fmmods.com/download-center/mega.php").then(urlResponse => {
+        const $ = cheerio.load(urlResponse.data)
+        $('div.su-button-center').each((i,element)=> {
+            const link = $(element).find("a").attr("href");
+            list.push({
+                name: link.split('/')[4].replace('.', '_').replace('_apk', '.apk'),
+                link: link
+            });
+        })
+        const result = {}
+        result.com_whatsapp = list && list[0] ? list[0] : undefined
+        result.com_fmwhatsapp = list && list[1] ? list[1] : undefined
+        result.com_gbwhatsapp = list && list[2] ? list[2] : undefined
+        result.com_yowhatsapp = list && list[3] ? list[3] : undefined
+        
+        console.log(result);
+        return result;
     })
-    const result = {}
-    result.com_whatsapp = list && list[0] ? list[0] : undefined
-    result.com_fmwhatsapp = list && list[1] ? list[1] : undefined
-    result.com_gbwhatsapp = list && list[2] ? list[2] : undefined
-    result.com_yowhatsapp = list && list[3] ? list[3] : undefined
-    
-    console.log(result);
-    return result;
-})}
+}
 
 module.exports = { mods }
